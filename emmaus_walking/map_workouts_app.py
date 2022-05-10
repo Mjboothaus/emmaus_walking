f04b37c29adb1e49d6ef66d410e8d824aacc4bc2
Michael Booth <michael@databooth.com.au>
2022-05-10 15:30:24 +1000

    Clean up

--- emmaus_walking/map_workouts_app.py
+++ emmaus_walking/map_workouts_app.py
@@ -5,29 +5,83 @@ from pathlib import Path
 from sqlite_utils import Database
 from streamlit_folium import folium_static
 import folium
+from st_aggrid import AgGrid
 
 st.set_page_config(layout="wide")
 
 def plot_walk_points(walk_points, map_handle, linecolour, linewidth):
     folium.PolyLine(walk_points, color=linecolour, weight=linewidth).add_to(map_handle)
 
-db = Database(Path("/Users/mjboothaus/data/healthkit/healthkit_2021_07_31.db"))
+# db = Database(Path("/Users/mjboothaus/data/healthkit/healthkit_2021_07_31.db"))
+
+db = Database(Path("/Users/mjboothaus/icloud/Data/apple_health_export/healthkit_db_2022_04_28.sqlite"))
 
 # Load some example data.
 DATA_URL = Path("nbs/walk_info_df.xlsx")
-data = st.cache(pd.read_excel)(DATA_URL)
+data = pd.read_excel(DATA_URL, parse_dates=["start_datetime"])
+
+df = data.copy()
+df.sort_values(by="start_datetime", inplace=True)
+df.reset_index(inplace=True)
+# st.write(df.dtypes)
+df["index"] = df.index
 
 # Select some rows using st.multiselect. This will break down when you have >1000 rows.
-st.write('### Full Dataset', data)
-selected_indices = st.multiselect('Select rows:', data.index)
 
+
+display_columns = [
+  "index",
+  "start_datetime",
+  # "start_latitude",
+  # "start_longitude",
+  # "start_altitude",
+  # "start_speed",
+  #"walk_group",
+  "workout_id",
+  #"finish_datetime",
+  #"finish_latitude",
+  #"finish_longitude",
+  #"finish_altitude",
+  #"finish_speed",
+  "start_location",
+  "finish_location",
+  "elapsed_time_hours",
+  # "duration_minutes",
+  "totaldistance_km"]
+  # "totalenergyburned_kJ",
+  # "sourceName",
+  # "sourceVersion",
+  # "startDate",
+  # "endDate",
+  # "metadata_HKWeatherTemperature",
+  # "metadata_HKWeatherHumidity",
+  # "metadata_HKElevationAscended",
+  # "metadata_HKAverageMETs",
+  # "index"]
+
+
+#st.write('### Full Dataset', data)
+st.markdown("### Full Dataset")
+grid = AgGrid(df[display_columns], editable=True)
+grid_df = grid["data"]
+
+selected_indices = st.multiselect('Select rows:', df.index)
+
+walk_group_options = ["nan", "GNW1", "GWW1"]
 if selected_indices != []:
 
     st.write('### Selected Rows')
 
     for row in selected_indices:
-        selected_row = data.loc[row]
+        selected_row = df[display_columns].loc[row]
+        print(selected_row)
         st.write(selected_row)
+
+        walk_group = st.selectbox("Walk group?", walk_group_options)
+        # st.write(walk_group)
+
+        if walk_group != "nan":
+            df["walk_group"].loc[row] = walk_group
 
         query = 'SELECT latitude, longitude FROM workout_points WHERE workout_id = "'
         query += selected_row['workout_id'] + '"'
